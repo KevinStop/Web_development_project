b55138b10b42ba503bf368f47ba3ef9ac5545a75
KevinStop <84851140+KevinStop@users.noreply.github.com>
2023-06-03 14:53:01 -0500

    Update web.php
    Added paths to controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\LibrosController;
+use App\Http\Controllers\PrestamosController;
 use Illuminate\Support\Facades\Route;
 
 /*
