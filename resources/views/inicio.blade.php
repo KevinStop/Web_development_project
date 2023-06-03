67dff536a1c081b6ed28dba954d1b0ec9b21a5e9
KevinStop <84851140+KevinStop@users.noreply.github.com>
2023-06-03 14:35:48 -0500

    creating controllers and models
    Using the console, the files for the models and controllers were created

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.index')
+
+@section('Titulo Pagina', 'Biblioteca')
+
+@section('contenido')
+    <div class="row mt-3">
+        <table class="table">
+            <thead>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Proveedor</th>
+                <th>Fecha Elaboracion</th>
+            </thead>
+            <tbody>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UsuariosController::class, 'index']) -> name ('usuarios.index');
+
