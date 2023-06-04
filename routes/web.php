7b5d86fd898e4d21aeaaca2533f3314d3621c15b
AlexSG <84851263+SteveenStopper@users.noreply.github.com>
2023-06-03 20:27:37 -0500

    Actualizacion
    Rutas

--- routes/web.php
+++ routes/web.php
@@ -18,38 +18,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('inicio');});
 
-<<<<<<< HEAD
-//Usuarios
-=======
-//rutas para usuarios
->>>>>>> 70c0c3efa5b32d83055401feaf4a805997c0b958
+// Rutas para usuarios
 Route::get('/usuarios', [UsuariosController::class, 'index'])->name('usuarios');
-Route::get('/create', [UsuariosController::class, 'create']) -> name ('usuarios.create');
-Route::get('/edit/{id}', [UsuariosController::class, 'edit']) -> name ('usuarios.edit');
-Route::post('/store', [UsuariosController::class, 'store']) -> name ('usuarios.store');
-Route::post('/update/{id}', [UsuariosController::class, 'update']) -> name ('usuarios.update');
-Route::post('/destroy/{id}', [UsuariosController::class, 'destroy']) -> name ('usuarios.destroy');
+Route::get('/usuarios/create', [UsuariosController::class, 'create'])->name('usuarios.create');
+Route::get('/usuarios/edit/{id}', [UsuariosController::class, 'edit'])->name('usuarios.edit');
+Route::post('/usuarios/store', [UsuariosController::class, 'store'])->name('usuarios.store');
+Route::post('/usuarios/update/{id}', [UsuariosController::class, 'update'])->name('usuarios.update');
+Route::post('/usuarios/destroy/{id}', [UsuariosController::class, 'destroy'])->name('usuarios.destroy');
 
-<<<<<<< HEAD
-//Libros
-=======
-//rutas para libros
->>>>>>> 70c0c3efa5b32d83055401feaf4a805997c0b958
+// Rutas para libros
 Route::get('/libros', [LibrosController::class, 'index'])->name('libros');
-Route::get('/create_libros', [LibrosController::class, 'create']) -> name ('libros.create_libros');
-Route::get('/edit_libros/{id}', [LibrosController::class, 'edit']) -> name ('libros.edit_libros');
-Route::post('/store_libros', [LibrosController::class, 'store']) -> name ('libros.store_libros');
-Route::post('/update_libros/{id}', [LibrosController::class, 'update']) -> name ('libros.update_libros');
-Route::post('/destroy_libros/{id}', [LibrosController::class, 'destroy']) -> name ('libros.destroy_libros');
+Route::get('/libros/create', [LibrosController::class, 'create'])->name('libros.create');
+Route::get('/libros/edit/{id}', [LibrosController::class, 'edit'])->name('libros.edit');
+Route::post('/libros/store', [LibrosController::class, 'store'])->name('libros.store');
+Route::post('/libros/update/{id}', [LibrosController::class, 'update'])->name('libros.update');
+Route::post('/libros/destroy/{id}', [LibrosController::class, 'destroy'])->name('libros.destroy');
 
-<<<<<<< HEAD
-//Prestamos
-=======
-//rutas para prestamos
->>>>>>> 70c0c3efa5b32d83055401feaf4a805997c0b958
+// Rutas para préstamos
 Route::get('/prestamos', [PrestamosController::class, 'index'])->name('prestamos');
-Route::get('/create', [PrestamosController::class, 'create']) -> name ('prestamos.create');
-Route::get('/edit/{id}', [PrestamosController::class, 'edit']) -> name ('prestamos.edit');
-Route::post('/store', [PrestamosController::class, 'store']) -> name ('prestamos.store');
-Route::post('/update/{id}', [PrestamosController::class, 'update']) -> name ('prestamos.update');
-Route::post('/destroy/{id}', [PrestamosController::class, 'destroy']) -> name ('prestamos.destroy');
+Route::get('/prestamos/create', [PrestamosController::class, 'create'])->name('prestamos.create');
+Route::get('/prestamos/edit/{id}', [PrestamosController::class, 'edit'])->name('prestamos.edit');
+Route::post('/prestamos/store', [PrestamosController::class, 'store'])->name('prestamos.store');
+Route::post('/prestamos/update/{id}', [PrestamosController::class, 'update'])->name('prestamos.update');
+Route::post('/prestamos/destroy/{id}', [PrestamosController::class, 'destroy'])->name('prestamos.destroy');
