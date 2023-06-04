d7e3c70bdec197852678b58b3b77e6dd3ae92142
AlexSG <84851263+SteveenStopper@users.noreply.github.com>
2023-06-03 21:01:32 -0500

    Super Avance
    va viento en popa

--- resources/views/ing_Prestamo.blade.php
+++ resources/views/ing_Prestamo.blade.php
@@ -2,25 +2,60 @@
 
 @section('Titulo Pagina', 'Ingresar Prestamos')
 
+<style>
+.custom-input {
+    max-width: 450px;
+}
+</style>
+
 @section('contenido')
-        <div class="row mt-3">
-            <h3 style="">Ingresar Prestamos</h3>
-            <form action="{{route("prestamos.store")}}" method="post">
-                @csrf
-                <label for="">ID Usuario</label>
-                <input type="text" name="usuario_id" class="form-control" required>
-                <br>
-                <label for="">ID Libro</label>
-                <input type="text" name="libro_id" class="form-control" required>
-                <br>
-                <label for="">Fecha Prestamo</label>
-                <input type="text" name="fecha_prestamo" class="form-control" required>
-                <br>
-                <label for="">Fecha Devolucion</label>
-                <input type="date" name="fecha_devolucion" class="form-control" required>
-                <br>
-                <a href="{{route("prestamos")}}" class="btn btn-secondary">Regresar</a>
+    <div class="row mt-3">
+        <h3 style="margin-bottom: 2%">Ingresar Prestamos</h3>
+        <form action="{{ route('prestamos.store') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="usuario_id" style="margin-bottom: 1%">ID Usuario</label>
+                <input type="number" id="usuario_id" name="usuario_id" class="form-control custom-input" required>
+                <div id="usuario_id_error" class="text-danger" style="display: none;">No puede ingresar números
+                    negativos.</div>
+            </div><br>
+
+            <div class="form-group">
+                <label for="libro_id" style="margin-bottom: 1%">ID Libro</label>
+                <input type="number" id="libro_id" name="libro_id" class="form-control custom-input" required>
+                <div id="libro_id_error" class="text-danger" style="display: none;">No puede ingresar números
+                    negativos.</div>
+            </div><br>
+
+            <div class="form-group">
+                <label for="fecha_prestamo" style="margin-bottom: 1%">Fecha Prestamo</label>
+                <input type="date" id="fecha_prestamo" name="fecha_prestamo" class="form-control custom-input" required>
+            </div><br>
+
+            <div class="form-group">
+                <label for="fecha_devolucion" style="margin-bottom: 1%">Fecha Devolucion</label>
+                <input type="date" id="fecha_devolucion" name="fecha_devolucion" class="form-control custom-input" required>
+            </div><br>
+
+            <div class="form-group">
+                <a href="{{ route('prestamos') }}" class="btn btn-secondary">Regresar</a>
                 <button class="btn btn-primary">Agregar</button>
-            </form>
-        </div>
-    @endsection
+            </div><br>
+        </form>
+    </div>
+
+    <script>
+        document.getElementById('usuario_id').addEventListener('input', function() {
+            var usuarioIdInput = document.getElementById('usuario_id');
+            var usuarioIdError = document.getElementById('usuario_id_error');
+            usuarioIdError.style.display = (usuarioIdInput.value < 0) ? 'block' : 'none';
+        });
+
+        document.getElementById('libro_id').addEventListener('input', function() {
+            var libroIdInput = document.getElementById('libro_id');
+            var libroIdError = document.getElementById('libro_id_error');
+            libroIdError.style.display = (libroIdInput.value < 0) ? 'block' : 'none';
+        });
+    </script>
+    </div>
+@endsection
