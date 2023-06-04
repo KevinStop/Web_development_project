cd77648eed90447fb7db44a648df30662e282de3
ovavellan <oscaravellan2000@gmail.com>
2023-06-04 01:07:07 -0500

    estilos
    modificación de estilso

--- resources/views/libros_actualizar.blade.php
+++ resources/views/libros_actualizar.blade.php
@@ -2,6 +2,12 @@
 
 @section('pageTitle', 'Actualizar Libro')
 
+<style>
+    .custom-input {
+        width: 300px;
+    }
+</style>
+
 @section('contenido')
     <div class="row mt-3">
         <form action="{{ route('libros.update', $libro->id) }}" method="POST">
@@ -9,25 +15,27 @@
             @method('PUT')
             <div class="form-group mb-3">
                 <label for="titulo">Título:</label>
-                <input type="text" name="titulo" id="titulo" class="form-control" value="{{ $libro->titulo }}">
+                <input type="text" name="titulo" id="titulo" class="form-control custom-input" value="{{ $libro->titulo }}">
             </div>
             <div class="form-group mb-3">
                 <label for="autor">Autor:</label>
-                <input type="text" name="autor" id="autor" class="form-control" value="{{ $libro->autor }}">
+                <input type="text" name="autor" id="autor" class="form-control custom-input" value="{{ $libro->autor }}">
             </div>
             <div class="form-group mb-3">
                 <label for="anio_publicacion">Año de publicación:</label>
-                <input type="text" name="anio_publicacion" id="anio_publicacion" class="form-control" value="{{ $libro->anio_publicacion }}">
+                <input type="text" name="anio_publicacion" id="anio_publicacion" class="form-control custom-input" value="{{ $libro->anio_publicacion }}">
                 @error('anio_publicacion')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group mb-3">
                 <label for="categoria">Categoría:</label>
-                <input type="text" name="categoria" id="categoria" class="form-control" value="{{ $libro->categoria }}">
+                <input type="text" name="categoria" id="categoria" class="form-control custom-input" value="{{ $libro->categoria }}">
             </div>
-            <button type="submit" class="btn btn-primary mb-4">Actualizar</button>
-            <a href="{{ route('libros') }}" class="btn btn-secondary mb-3">Regresar</a>
+            <div class="d-flex">
+                <button type="submit" class="btn btn-primary me-2">Actualizar</button>
+                <a href="{{ route('libros') }}" class="btn btn-secondary">Regresar</a>
+            </div>
         </form>
     </div>
 @endsection
